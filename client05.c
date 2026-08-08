8af5fbe2b15330b8735d1806a7e21c4d2b37ce64
code-komal <komalkumaribgs002007@gmail.com>
2026-08-08 11:56:44 +0530

    Add files via upload

---
+++ client05.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8081
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE];
+    char input[BUFFER_SIZE];
+
+    // Create Socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    // Send multiple requests
+    while (1) {
+        printf("Enter command: ");
+        fgets(input, BUFFER_SIZE, stdin);
+
+        input[strcspn(input, "\n")] = 0;
+
+        send(sock, input, strlen(input), 0);
+
+        if (strcmp(input, "bye") == 0)
+            break;
+
+        memset(buffer, 0, BUFFER_SIZE);
+
+        int valread = read(sock, buffer, BUFFER_SIZE - 1);
+
+        if (valread <= 0)
+            break;
+
+        buffer[valread] = '\0';
+
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+
+    return 0;
+}
